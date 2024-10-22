5629d6133e469eb7dd7ac33d6eb6164db7411c2e
Farhan Khalid <rj.farhan4232@gmail.com>
2024-10-22 22:32:38 +0500

    Completed the application

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("article/<int:id>", views.article, name="article"),
+    path("author/<int:id>", views.author, name="author"),
+    path("article", views.add_article, name="add_article"),
+    path("article/edit/<int:id>", views.edit_article, name="edit_article"),
+    path("author", views.add_author, name="add_author"),
+    path("author/edit/<int:id>", views.edit_author, name="edit_author"),
 ]
